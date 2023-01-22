48f516fa412e798f0618cd016b576400a1c01c90
jp-data <joaopaulo94.economia@gmail.com>
2023-01-22 02:02:37 -0300

    Rota habits - registro de novo hábito

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,40 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from "./lib/prisma"
+import { z } from 'zod'
+import dayjs from 'dayjs'
 
 export async function appRoutes(app: FastifyInstance) {
 
-    app.get( '/', async () => {
-        //acessando os hábitos
-        const habits = await prisma.habit.findMany()
-    
-        return habits
-    
+    //adiciona um hábito
+    app.post('/habits', async (request) => {
+        //zod para validar dados
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6)
+            )
+        })
+
+        const { title, weekDays } = createHabitBody.parse(request.body)
+
+        //permitindo inserir um hábito que passe a valer no mesmo dia
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: new Date(),
+                weekDays: {
+                    create: weekDays.map( weekDay => {
+                        return {
+                            week_day: weekDay,
+                        }
+                    })
+                }
+            }
+        })
+
+
     })
 }
 
